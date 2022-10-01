11eb85781721bb78480b97cec711ad9cc7160c2a
Dindinha <indynha.w@gmail.com>
2022-10-01 20:22:31 -0300

    Bug fixes

--- HumanCoder/libft/ft_strtrim.c
+++ HumanCoder/libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iw90 <iw90@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 15:11:25 by inwagner          #+#    #+#             */
-/*   Updated: 2022/10/01 15:11:45 by iw90             ###   ########.fr       */
+/*   Updated: 2022/10/01 20:19:31 by iw90             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,18 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	i;
 	size_t	j;
 	size_t	size;
-	int	validator;
-	int	validator2;
 
 	size = ft_strlen(s1);
 	i = 0;
 	j = size - 1;
-	validator = 1;
-	validator2 = 1;
-	while (i < j && (validator2 || validator))
+	while (i < j && (ft_strchr(set, s1[i]) || ft_strchr(set, s1[j])))
 	{
-		if (!ft_strchr(set, s1[i]))
-			validator = 0;
-		else
+		if (ft_strchr(set, s1[i]))
 		{
 			size--;
 			i++;
 		}
-		if (!ft_strchr(set, s1[j]))
-			validator2 = 0;
-		else
+		if (ft_strchr(set, s1[j]))
 		{
 			size--;
 			j--;
