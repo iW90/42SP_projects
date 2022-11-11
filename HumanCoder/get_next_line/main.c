5d628353e32be56a4f63029f2c75934dc1e434ba
Dindinha <indynha.w@gmail.com>
2022-11-11 20:45:43 -0300

    gnl v1

--- HumanCoder/get_next_line/main.c
+++ HumanCoder/get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: inwagner <inwagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:09:26 by inwagner          #+#    #+#             */
-/*   Updated: 2022/11/11 18:59:02 by inwagner         ###   ########.fr       */
+/*   Updated: 2022/11/11 20:45:28 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ int	main(int argc, char **argv)
 // ./a.out /dev/tty
 	/* standart input: Recebe uma entrada que nós digitamos e 
 	nossa GNL deve imprimir o mesmo. Interrompa com ctrl+c */
+//./a.out file
 
 //read retorna quantidade de bytes lidos
 //read coloca conteúdo lido no line mallocado passado por parâmetro
